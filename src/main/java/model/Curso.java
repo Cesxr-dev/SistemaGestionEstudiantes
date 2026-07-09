e0c55dd9de5efc16b03f9010111e0e88b67279e1
Julian,n <juliandanielramirez9@gmail.com>
2026-07-09 00:48:12 -0700

    toString Curso.java

--- src/main/java/model/Curso.java
+++ src/main/java/model/Curso.java
@@ -27,6 +27,11 @@ public class Curso implements Comparable<Curso> {
     }
     
     @Override
+    public String toString() {
+        return "Curso[" + "clave: " + clave + ", " + "nombre: " + nombre + "]";
+    }
+    
+    @Override
     public int compareTo(Curso o) {
         return this.clave.compareTo(o.clave);
     }
