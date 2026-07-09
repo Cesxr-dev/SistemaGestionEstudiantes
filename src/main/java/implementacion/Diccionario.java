ec6d5e8141f2ce54ae0fe00ac7961a9653ad2343
Cesxr_dev <cesarqmt115@gmail.com>
2026-07-09 07:48:37 -0700

    Clase Diccionario y Metodo agregar, eliminar, listar cursos implementados + validaciones

---
+++ src/main/java/implementacion/Diccionario.java
@@ -0,0 +1,107 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package implementacion;
+
+/**
+ *
+ * @author Cesar Demian Quiroz Montijo 252975
+ */
+public class Diccionario<K, V> {
+
+    private static class HashNode<K, V> {
+        K key;
+        V value;
+        HashNode<K, V> next;
+
+        public HashNode(K key, V value) {
+            this.key = key;
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    private HashNode<K, V>[] buckets;
+    private int capacidad;
+    private int tamaño;
+
+    @SuppressWarnings("unchecked")
+    public Diccionario(int capacidad) {
+        this.capacidad = capacidad;
+        this.buckets = new HashNode[capacidad];
+        this.tamaño = 0;
+    }
+
+    private int obtenerIndiceBucket(K key) {
+        return Math.abs(key.hashCode() % capacidad);
+    }
+
+    public void put(K key, V value) throws Exception {
+        int bucketIndex = obtenerIndiceBucket(key);
+        HashNode<K, V> head = buckets[bucketIndex];
+
+        HashNode<K, V> temp = head;
+        while (temp != null) {
+            if (temp.key.equals(key)) {
+                throw new Exception("La clave del curso ya existe en el diccionario.");
+            }
+            temp = temp.next;
+        }
+
+        HashNode<K, V> nuevoNodo = new HashNode<>(key, value);
+        nuevoNodo.next = head;
+        buckets[bucketIndex] = nuevoNodo;
+        tamaño++;
+    }
+
+    public V get(K key) {
+        int bucketIndex = obtenerIndiceBucket(key);
+        HashNode<K, V> head = buckets[bucketIndex];
+
+        while (head != null) {
+            if (head.key.equals(key)) {
+                return head.value;
+            }
+            head = head.next;
+        }
+        return null;
+    }
+
+    public V remove(K key) {
+        int bucketIndex = obtenerIndiceBucket(key);
+        HashNode<K, V> head = buckets[bucketIndex];
+        HashNode<K, V> prev = null;
+
+        while (head != null) {
+            if (head.key.equals(key)) {
+                break;
+            }
+            prev = head;
+            head = head.next;
+        }
+
+        if (head == null) return null;
+
+        tamaño--;
+        if (prev != null) {
+            prev.next = head.next;
+        } else {
+            buckets[bucketIndex] = head.next;
+        }
+
+        return head.value;
+    }
+
+    // Metodo para imprimir o listar los cursos sin exponer los nodos al exterior
+    public void imprimirContenido() {
+        for (int i = 0; i < capacidad; i++) {
+            HashNode<K, V> temp = buckets[i];
+            while (temp != null) {
+                // Aquí temp.value sera el objeto Curso, se imprimira usando su toString()
+                System.out.println(temp.value); 
+                temp = temp.next;
+            }
+        }
+    }
+}
--- src/main/java/services/GestionSistemaService.java
+++ src/main/java/services/GestionSistemaService.java
@@ -5,18 +5,60 @@
 package services;
 
 import implementacion.BinarySearchTree;
+import implementacion.Diccionario;
+import model.Curso;
 import model.Estudiante;
 
 /**
  *
- * @author demib
+ * @author Cesar Demian Quiroz Montijo 252975
  */
 public class GestionSistemaService {
     private final BinarySearchTree<Estudiante> arbolEstudiantes = new BinarySearchTree<>();
+    private final Diccionario<String,Curso> tablaCursos = new Diccionario<>(20);
     
+    /**
+     * Metodo para agregar un estudiante al arbol BST
+     * @param est Estudiante a agregar
+     */
     public void agregarEstudiante(Estudiante est){
         arbolEstudiantes.insertar(est);
         
-        arbolEstudiantes.inOrder();
+        arbolEstudiantes.inOrder(); // metodo solo para comprobar que funciona el insertar
     }
+    
+    
+    /**
+     * Metodo para agregar un curso al Diccionario/Tabla de cursos
+     * @param c Curso a insertar
+     * @throws Exception si clave del curso ya existe en el diccionario.
+     */
+    public void agregarCurso(Curso c) throws Exception{
+        tablaCursos.put(c.getClave(), c);
+    }
+    
+    
+    /**
+     * Metodo para eliminar un curso del Catalogo de Cursos
+     * @param clave
+     * @throws Exception si la clave no se encuentra en el sistema
+     */
+    public void eliminarCurso(String clave) throws Exception{
+        
+        
+        Curso eliminado = tablaCursos.remove(clave);
+        
+        if(eliminado == null){
+            throw new Exception("El curso con la clave '" + clave + "' no se encuentra registrado en el sistema.");
+        }
+    }
+    
+    /**
+     * Lista todos los cursos del catalogo de cursos
+     */
+    public void listarCursos(){
+        tablaCursos.imprimirContenido();
+    }
+    
+    
 }
